2c8f3a2ed46006908c0eafdbe08f29743d76d3af
unknown <utkarshmaheria@gmail.com>
2020-08-28 02:46:15 +0530

    signup request created

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,8 @@
 //module.exports.functionName = function(req, res) {}
 
 module.exports.home = function(req, res){
-    
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
     return res.render('home', {
         title: "Home"
     });
