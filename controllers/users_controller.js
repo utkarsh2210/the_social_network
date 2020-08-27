53a61ab2fd21066b6822bbf55390ec81f29fd573
unknown <utkarshmaheria@gmail.com>
2020-08-28 02:00:38 +0530

    sign up & sign in views created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,27 @@ module.exports.profile = function(req, res){
         title: "Profile Page"
     });
 }
+
+// render the sign up page
+module.exports.signUp = function(req, res){
+    res.render('user_sign_up', {
+        title: "Network | Sign Up"
+    });
+}
+
+// render the sign in page
+module.exports.signIn = function(req, res){
+    res.render('user_sign_in', {
+        title: "Network | Sign In"
+    });
+}
+
+// get the sign up data
+module.exports.create = function(req, res){
+    //TODO later
+}
+
+// sign in and create a session for the user
+module.exports.createSession = function(req, res){
+    //TODO later
+}
