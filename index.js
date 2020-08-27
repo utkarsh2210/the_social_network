b2f9c196a6edd3cbc4afbab5a3603b0e40816be0
unknown <utkarshmaheria@gmail.com>
2020-08-28 00:52:38 +0530

    created mongoose file

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+app.use(express.static('./assets'));
+
+// extract style and scripts from sub pages into layout 
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.use(expressLayouts);
 
