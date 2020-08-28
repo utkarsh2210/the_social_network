24555c09f93c7f2c9b033b809103715e4f3f375f
unknown <utkarshmaheria@gmail.com>
2020-08-28 23:52:08 +0530

    mongo-store setup done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-
+const MongoStore = require('connect-mongo')(session);
 
 app.use(express.urlencoded());
 
@@ -27,7 +27,7 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
+// mongo store is used to store the session cookie in db
 app.use(session({
     name: 'thesocialnetwork',
     // ToDo change the secret before deployment in production mode
@@ -36,7 +36,16 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongo db setup ok');
+        }
+    )
 }));
 
 app.use(passport.initialize());
