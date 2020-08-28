71bb204bac3468b590498446df605b27a2f4ca6a
unknown <utkarshmaheria@gmail.com>
2020-08-28 23:42:23 +0530

    show data on profile page & handle sign-up/in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,21 +3,28 @@ const User = require('../models/user');
 
 
 module.exports.profile = function(req, res){
-    res.render('user_profile', { 
+    return res.render('user_profile', { 
         title: "Profile Page"
     });
 }
 
 // render the sign up page
 module.exports.signUp = function(req, res){
-    res.render('user_sign_up', {
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    
+    return res.render('user_sign_up', {
         title: "Network | Sign Up"
     });
 }
 
 // render the sign in page
 module.exports.signIn = function(req, res){
-    res.render('user_sign_in', {
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    return res.render('user_sign_in', {
         title: "Network | Sign In"
     });
 }
