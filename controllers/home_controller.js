3c4f566dc277971fc006881913c8f83913e75979
unknown <utkarshmaheria@gmail.com>
2020-08-30 01:19:41 +0530

    populated users to display the author as well as the post on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,23 @@
+const Post = require('../models/posts');
+
 //module.exports.functionName = function(req, res) {}
 
 module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Home",
+    //         posts: posts
+    //     });
+    // });
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Home",
+            posts: posts
+        });
     });
 }
