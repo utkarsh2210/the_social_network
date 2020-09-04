8b732f23b63a018edb7bf2bde8de6fd9f9a20cda
unknown <utkarshmaheria@gmail.com>
2020-09-04 16:17:54 +0530

    api to delete a post from db (no auth)

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,4 +1,5 @@
 const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
 
 module.exports.index = async function(req, res){
 
@@ -16,4 +17,27 @@ module.exports.index = async function(req, res){
             message: 'List of Posts',
             posts: posts
         });
+}
+
+
+module.exports.destroy = async function(req, res){
+    
+    try{
+        let post = await Post.findById(req.params.id);
+        // if(post.user == req.user.id){
+            post.remove();
+
+            await Comment.deleteMany({post: req.params.id});
+
+            return res.json(200, {
+                message: 'Posts & comments deleted'
+            });
+        // }else{
+        //     return res.redirect('back');
+        // }
+    }catch(err){
+        return res.json(500, {
+            message: 'Internal server error'
+        });
+    }
 }
