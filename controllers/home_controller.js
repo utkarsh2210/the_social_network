c31fc97fb51b855232425164459e96a9ca4add1c
unknown <utkarshmaheria@gmail.com>
2020-09-08 23:59:41 +0530

    added frontend for displaying friends

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -26,15 +26,44 @@ module.exports.home = async function(req, res){
             populate: {
                 path: 'likes'
             }
-        }).populate('comments')
-        .populate('likes');
+        }).populate('likes');
 
         let users = await User.find({});
+
+        let friends = new Array();
+        if (req.user)
+        {
+            let all_friendships = await Friendship.find({ $or: [{ from_user: req.user._id }, { to_user: req.user._id }] })
+                .populate('from_user')
+                .populate('to_user');
+
+            for (let all of all_friendships)
+            {
+                if (all.from_user._id.toString() == req.user._id.toString())
+                {
+                    friends.push({
+                        friend_name: all.to_user.name,
+                        friend_id: all.to_user._id,
+                        friend_avatar: all.to_user.avatar
+                    });
+                }
+                else if (all.to_user._id.toString() == req.user._id.toString())
+                {
+                    friends.push({
+                        friend_name: all.from_user.name,
+                        friend_id: all.from_user._id,
+                        friend_avatar: all.from_user.avatar
+                    });
+                }
+            }
+        }
+
 
         return res.render('home', {
             title: "Home",
             posts: posts,
-            all_users: users
+            all_users: users,
+            all_friends: friends
         }); 
 
     }catch(err){
