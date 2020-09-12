545bd29b6f41009e58cf885ed2843736e570ef7a
unknown <utkarshmaheria@gmail.com>
2020-09-12 19:57:46 +0530

    corrected some edge cases & added a background

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -101,7 +101,7 @@ module.exports.update = async function(req, res){
 // render the sign up page
 module.exports.signUp = function(req, res){
     if (req.isAuthenticated()){
-        return res.redirect('/users/profile');
+        return res.redirect(`/users/profile/${req.user._id}`);
     }
     
     return res.render('user_sign_up', {
@@ -112,7 +112,7 @@ module.exports.signUp = function(req, res){
 // render the sign in page
 module.exports.signIn = function(req, res){
     if (req.isAuthenticated()){
-        return res.redirect('/users/profile');
+        return res.redirect(`/users/profile/${req.user._id}`);
     }
     return res.render('user_sign_in', {
         title: "Network | Sign In"
